60525be5c591a9f2f3fce635ac903effe27f041e
Loc Pham <loc.pham@logixtek.com>
2020-09-16 17:55:41 +0700

    add storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,10 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+    "@storybook/addon-knobs",
+    "@storybook/addon-events",
+    "@storybook/addon-notes/register-panel",
+    "@storybook/addon-storysource",
   ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,4 +3,11 @@ import "../src/index.css";
 // Configures Storybook to log the actions(onArchiveTask and onPinTask) in the UI.
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  // Storybook a11y addon configuration
+  a11y: {
+    // the target DOM element
+    element: "#root",
+    // sets the execution mode for the addon
+    manual: false,
+  },
 };
